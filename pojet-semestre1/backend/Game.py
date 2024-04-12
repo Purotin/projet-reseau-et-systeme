a22631607af18957d32a69921d54aa4493353447
unPoulpe <yayafeu2@gmail.com>
2024-04-12 13:47:21 +0200

    up

--- pojet-semestre1/backend/Game.py
+++ pojet-semestre1/backend/Game.py
@@ -335,3 +335,37 @@ class Game:
         print("Game loaded from " + pathToSaveFile)
 
         return True
+
+    def udpdateData(self, data):
+        with open(data, 'rb') as f:
+            try:
+                data = pickle.load(f)
+            except:
+                print("Error while loading save file")
+                return False
+            
+        old_grid_dict = self.grid.gridDict
+
+        for key, cell in old_grid_dict:
+            if cell.bobs:
+                for bob in cell.bobs:
+                    if bob.id in data:
+                        #update bob data
+                        if data[bob.id]['position']:
+                            bob.lastX = bob.currentX
+                            bob.lastY = bob.currentY
+                            bob.currentX = data[bob.id]['position'][0]
+                            bob.currentY = data[bob.id]['position'][1]
+                        bob.energy = data[bob.id]['energy'] if data[bob.id]['energy'] else bob.energy
+                        pass
+
+            elif cell.edibleObject:
+                if cell.edibleObject.id in data:
+                    if data[cell.edibleObject.id]['position']:
+                        cell.edibleObject.x = data[cell.edibleObject.id]['position'][0]
+                        cell.edibleObject.y = data[cell.edibleObject.id]['position'][1]
+                    cell.edibleObject.value = data[cell.edibleObject.id]['value'] if data[cell.edibleObject.id]['value'] else cell.edibleObject.value
+                    pass
+
+
+        # {"id_bob" : {"position" : x}}
